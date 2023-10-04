c7eccbed1daf8e14209bff9276ce285b35558a89
Mlabarbara <mlabarbara@gmail.com>
2023-10-04 15:06:59 -0600

    terraform format

--- main.tf
+++ main.tf
@@ -1,98 +1,98 @@
 terraform {
   required_providers {
     proxmox = {
-      source = "telmate/proxmox"
+      source  = "telmate/proxmox"
       version = "2.9.14"
     }
   }
 }
 # provider is the infrastructer that we will be building on; in this case it is my r630 proxmox server
 provider "proxmox" {
-  pm_api_url = var.pm_api_url
-  pm_api_token_id = var.pm_token_id
+  pm_api_url          = var.pm_api_url
+  pm_api_token_id     = var.pm_token_id
   pm_api_token_secret = var.pm_token_secret
-  pm_tls_insecure = true
+  pm_tls_insecure     = true
 }
 
 # now we start to define the resources that we want to build
 # the resource name I believe is defined in the provider so it has to be proxmox_vm_qemu
 resource "proxmox_vm_qemu" "kube-server" {
-  count = 1
-  name = "kuber-server-0${count.index + 1}"
+  count       = 1
+  name        = "kuber-server-0${count.index + 1}"
   target_node = "r630-pve"
-  vmid = "100${count.index + 1}"
+  vmid        = "100${count.index + 1}"
 
-  clone = "ubuntu-2004-cloudinit-template"
+  clone      = "ubuntu-2004-cloudinit-template"
   full_clone = true
-  
-  agent = 1
-  os_type = "cloud-init"
-  cores = 4
-  sockets = 1
-  cpu = "host"
-  memory = 4096
-  scsihw = "virtio-scsi-single"
+
+  agent    = 1
+  os_type  = "cloud-init"
+  cores    = 4
+  sockets  = 1
+  cpu      = "host"
+  memory   = 4096
+  scsihw   = "virtio-scsi-single"
   bootdisk = "scsi0"
 
   disk {
-    slot = 0
-    size = "10G"
-    type = "scsi"
-    storage = "nvme001"
+    slot     = 0
+    size     = "10G"
+    type     = "scsi"
+    storage  = "nvme001"
     iothread = 1
-    discard = "on"
-    ssd = 1
+    discard  = "on"
+    ssd      = 1
   }
   network {
-    model = "virtio"
+    model  = "virtio"
     bridge = "vmbr0"
   }
   network {
-    model = "virtio"
+    model  = "virtio"
     bridge = "vmbr1"
   }
   ipconfig0 = "ip=192.168.10${count.index + 1}.${count.index + 1}/16,gw=192.168.10.1"
-  ipconfig1 = "ip=10.10.10.1${count.index + 1}/24" 
+  ipconfig1 = "ip=10.10.10.1${count.index + 1}/24"
 
-  sshkeys= var.ssh_key
+  sshkeys = var.ssh_key
 }
 resource "proxmox_vm_qemu" "kube-node" {
-  count = 2
-  name = "kube-node-0${count.index + 1}"
+  count       = 2
+  name        = "kube-node-0${count.index + 1}"
   target_node = "r630-pve"
-  vmid = "101${count.index + 1}"
+  vmid        = "101${count.index + 1}"
 
-  clone = "ubuntu-2004-cloudinit-node"
+  clone      = "ubuntu-2004-cloudinit-node"
   full_clone = true
-  agent = 1
-  os_type = "cloud-int"
-  cores = 2
-  cpu = "host"
-  memory = 2048
-  sockets = 1
-  scsihw = "virtio-scsi-single"
-  bootdisk = "scsi0"
+  agent      = 1
+  os_type    = "cloud-int"
+  cores      = 2
+  cpu        = "host"
+  memory     = 2048
+  sockets    = 1
+  scsihw     = "virtio-scsi-single"
+  bootdisk   = "scsi0"
 
   disk {
-    slot = 0
-    size = "10G"
-    type = "scsi"
-    storage = "nvme002"
+    slot     = 0
+    size     = "10G"
+    type     = "scsi"
+    storage  = "nvme002"
     iothread = 1
-    discard = "on"
-    ssd = 1
+    discard  = "on"
+    ssd      = 1
   }
   network {
-    model = "virtio"
+    model  = "virtio"
     bridge = "vmbr0"
   }
   network {
-    model = "virtio"
+    model  = "virtio"
     bridge = "vmbr1"
   }
 
   ipconfig0 = "ip=192.168.2${count.index + 1}.10${count.index + 1}/16,gw=192.168.10.1"
   ipconfig1 = "ip=10.10.10.2${count.index + 1}/24"
-  sshkeys = var.ssh_key
-  
+  sshkeys   = var.ssh_key
+
 }
--- vars.tf
+++ vars.tf
@@ -5,7 +5,7 @@ variable "pm_token_id" {
   description = "This is the token ID for the user that has permissions to create VMs, etc..."
 }
 variable "pm_token_secret" {
- description = "This is the token secret for said user"
+  description = "This is the token secret for said user"
 }
 variable "ssh_key" {
   description = "The ssh keys that will be loaded onto the machines"
