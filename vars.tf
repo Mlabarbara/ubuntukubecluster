692f71ffaf4d8b010b2c6a79389f88a094987c75
Mlabarbara <mlabarbara@gmail.com>
2023-10-04 14:10:26 -0600

    add variables to vars.tf

---
+++ vars.tf
@@ -0,0 +1,12 @@
+variable "pm_api_url" {
+  description = "This is the URL to the proxmox server"
+}
+variable "pm_token_id" {
+  description = "This is the token ID for the user that has permissions to create VMs, etc..."
+}
+variable "pm_token_secret" {
+ description = "This is the token secret for said user"
+}
+variable "ssh_key" {
+  description = "The ssh keys that will be loaded onto the machines"
+}
